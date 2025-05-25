3e668abbf1b5417575f172049f6953bc399b6406
Leonardo2106 <leonardo.piassa07@gmail.com>
2025-05-25 15:05:26 -0300

    Adiciona estrutura inicial

---
+++ semestre-1/main.py
@@ -0,0 +1,51 @@
+"""
+- Objetivo da Sprint:
+
+Com base na lógica definida na Sprint 01, nesta etapa a equipe deverá:
+
+Codificar em Python a lógica desenvolvida (usando estruturas condicionais e entradas simuladas);
+Desenvolver uma simulação prática com Arduino, representando fisicamente o funcionamento dessa lógica (como apresentado nas aulas práticas).
+
+Essa sprint conecta a lógica digital à programação aplicada e à representação física de controle, proporcionando uma compreensão prática e visual do sistema desenvolvido.
+
+
+
+- Entregáveis obrigatórios:
+
+1. Algoritmo em Python
+
+Um código que represente o circuito lógico da Sprint 01
+Deve incluir:
+
+Definição de entradas (ex: input(), ou variáveis simuladas)
+Condicionais (if, and, or, not, etc.)
+Impressão do(s) resultado(s) com base na lógica (ex: "Motor ligado", "Alarme ativado", etc.)
+Comentar brevemente o funcionamento de cada parte do código
+
+2. Simulação com Arduino
+
+Representar a lógica em um circuito no Tinkercad Circuits ou outro simulador
+Deve incluir:
+
+Entradas digitais (botões, sensores)
+Saídas (LEDs, buzzer, servo motor, etc.)
+Códigos Arduino (usando if/else, digitalRead, digitalWrite)
+Exportar screenshots ou o link do circuito simulado
+Explicar o que representa cada parte do circuito físico em relação ao circuito lógico original
+
+
+
+- Critérios de Avaliação
+
+Critério | Peso
+
+Fidelidade da lógica do código em Python | 3,0
+
+Clareza e organização do código | 2,0
+
+Representação coerente da lógica no Arduino | 3,0
+
+Explicação e apresentação geral | 2,0
+"""
+
+print("Algoritmo em Python e Simulação com Arduino")
